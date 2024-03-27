ad76985af1201c14c4de15c1a5f62f0775ef509f
GiselleBarbosa <g88barbosa@gmail.com>
2024-03-27 10:55:49 -0300

    feat: adicionado swagger para documentacao da API

--- sgu-gerenciamento/src/main/java/gisellebarbosa/com/sgu/controller/FuncionarioController.java
+++ sgu-gerenciamento/src/main/java/gisellebarbosa/com/sgu/controller/FuncionarioController.java
@@ -19,16 +19,25 @@ import org.springframework.web.bind.annotation.RestController;
 import gisellebarbosa.com.sgu.model.Funcionario;
 import gisellebarbosa.com.sgu.repository.FuncionarioRepository;
 import gisellebarbosa.com.sgu.exception.ResourceNotFoundException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @CrossOrigin(origins = "http://localhost:4200/")
 @RestController
 @RequestMapping("/api/v1/")
+@Tag(name = "Funcionario Controller", description = "Endpoints para gerenciar funcionários")
 public class FuncionarioController {
     @Autowired
     private FuncionarioRepository funcionarioRepository;
 
-    // cadastra um novo funcionario
     @PostMapping("/funcionarios")
+    @Operation(summary = "Cadastrar um novo funcionário", description = "Este endpoint permite cadastrar um novo funcionário no sistema.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Funcionário cadastrado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Falha ao cadastrar funcionário: Verifique os dados fornecidos")
+    })
     public ResponseEntity<FuncionarioResponse> cadastraFuncionario(@RequestBody Funcionario funcionario) {
         Funcionario funcionarioSalvo = funcionarioRepository.save(funcionario);
         if (funcionarioSalvo.getId() != null) {
@@ -41,8 +50,8 @@ public class FuncionarioController {
         }
     }
 
-    // lista todos funcionarios
     @GetMapping("/funcionarios")
+    @Operation(summary = "Listar todos os funcionários", description = "Este endpoint retorna uma lista de todos os funcionários cadastrados no sistema.")
     public FuncionarioResponse listarTodosFuncionarios() {
         List<Funcionario> funcionarios = funcionarioRepository.findAll();
         if (funcionarios.isEmpty()) {
@@ -52,8 +61,12 @@ public class FuncionarioController {
         return new FuncionarioResponse(mensagem, funcionarios);
     }
 
-    // lista funcionarios por ID
     @GetMapping("/funcionarios/{id}")
+    @Operation(summary = "Listar um funcionário por ID", description = "Este endpoint retorna um funcionário específico com base no ID fornecido.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Funcionário encontrado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Não encontrado funcionário com o ID fornecido")
+    })
     public ResponseEntity<FuncionarioResponse> listaFuncionarioPorID(@PathVariable Long id) {
         Funcionario funcionario = funcionarioRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Não encontrado funcionário com id:" + id));
@@ -64,9 +77,12 @@ public class FuncionarioController {
         return ResponseEntity.ok(new FuncionarioResponse(mensagem, funcionariosEncontrados));
     }
 
-
-    // atualiza funcionarios
     @PutMapping("/funcionarios/{id}")
+    @Operation(summary = "Atualizar um funcionário", description = "Este endpoint permite atualizar as informações de um funcionário existente no sistema.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Funcionário atualizado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Não encontrado funcionário com o ID fornecido")
+    })
     public ResponseEntity<FuncionarioResponse> atualizaFuncionario(
             @PathVariable Long id,
             @RequestBody Funcionario funcionarioDetalhes) {
@@ -90,8 +106,12 @@ public class FuncionarioController {
         return ResponseEntity.ok(new FuncionarioResponse(mensagem, funcionariosAtualizados));
     }
 
-    // remove funcionarios
     @DeleteMapping("/funcionarios/{id}")
+    @Operation(summary = "Remover um funcionário", description = "Este endpoint permite remover um funcionário do sistema com base no ID fornecido.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Funcionário removido com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Não encontrado funcionário com o ID fornecido")
+    })
     public ResponseEntity<FuncionarioResponse> removeFuncionario(@PathVariable Long id) {
         Funcionario funcionario = funcionarioRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Não encontrado funcionário com id: " + id));
